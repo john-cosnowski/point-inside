2eb342624fd6b26f3acfbfb6dd411b982f949d71
John Cosnowski <jcosnowski@gmail.com>
2014-02-09 21:11:57 -0800

    Use JBOSS RestEasy

--- src/main/java/com/pi/controller/SearchCategoryController.java
+++ src/main/java/com/pi/controller/SearchCategoryController.java
@@ -1,21 +1,25 @@
 package com.pi.controller;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 import java.util.Collection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pi.dao.ProductsDAO;
 import com.pi.model.Product;
 
 @Controller
-@RequestMapping("/search/category")
+@Path("/search/category")
+@Produces(APPLICATION_JSON)
 public class SearchCategoryController {
 	
 	private static Logger LOG = LoggerFactory.getLogger("com.pi.controller.SearchCategoryController");
@@ -23,21 +27,23 @@ public class SearchCategoryController {
 	@Autowired
 	private ProductsDAO productsDAO;
 	
-	@RequestMapping(method=RequestMethod.GET, headers={"Accept=application/json"})
-	public @ResponseBody Collection<String> getAllCategories()  {
+	@GET
+	public Collection<String> getAllCategories()  {
 		LOG.info("getAllCategories()");
 		return productsDAO.getAllCategories();
 	}
 
-	@RequestMapping(value="{category}", method=RequestMethod.GET, headers={"Accept=application/json"})
-	public @ResponseBody Collection<Product> getCategoryProducts(@PathVariable("category") String category)  {
+	@GET
+	@Path("{category}")
+	public Collection<Product> getCategoryProducts(@PathParam("category") String category)  {
 		LOG.info(String.format("getCategoryProducts(%s)", category));
 		return productsDAO.getProductsByCategory(category);
 	}
 
-	@RequestMapping(value="{category}/keyword/{word}", method=RequestMethod.GET, headers={"Accept=application/json"})
-	public @ResponseBody Collection<Product> getProductsByCategoryAndKeyword(
-			@PathVariable("category") String category, @PathVariable("word") String word)  {
+	@GET
+	@Path("{category}/keyword/{word}")
+	public Collection<Product> getProductsByCategoryAndKeyword(
+			@PathParam("category") String category, @PathParam("word") String word)  {
 		LOG.info(String.format("getCategoryProducts(%s, %s)", category, word));
 		return productsDAO.getProductsByCategoryAndKeyword(category, word);
 	}
--- src/main/java/com/pi/controller/SearchKeywordController.java
+++ src/main/java/com/pi/controller/SearchKeywordController.java
@@ -1,21 +1,25 @@
 package com.pi.controller;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 import java.util.Collection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pi.dao.KeywordsDAO;
 import com.pi.model.Product;
 
 @Controller
-@RequestMapping("/search/keyword")
+@Path("/search/keyword")
+@Produces(APPLICATION_JSON)
 public class SearchKeywordController {
 
 	private static Logger LOG = LoggerFactory.getLogger("com.pi.controller.SearchKeywordController");
@@ -23,14 +27,14 @@ public class SearchKeywordController {
 	@Autowired
 	private KeywordsDAO keywordsDAO;
 	
-	@RequestMapping(method=RequestMethod.GET, headers={"Accept=application/json"})
-	public @ResponseBody Collection<String> getAllKeywords()  {
+	@GET
+	public Collection<String> getAllKeywords()  {
 		LOG.info(String.format("SearchKeywordController.getAllKeywords()"));
 		return keywordsDAO.getAllKeywords();
 	}
 	
-	@RequestMapping(value="{word}", method=RequestMethod.GET, headers={"Accept=application/json"})
-	public @ResponseBody Collection<Product> getProductsByKeyword(@PathVariable("word") String word)  {
+	@Path("{word}")
+	public Collection<Product> getProductsByKeyword(@PathParam("word") String word)  {
 		LOG.info(String.format("SearchKeywordController.getProductsByKeyword(%s)", word));
 		return keywordsDAO.getProductsByKeyword(word);
 	}
